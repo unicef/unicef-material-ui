01f6ed221dc47ae3c7724028a02d469f803183f5
Suresh <sevarthi.suresh@gmail.com>
2019-07-18 10:01:05 -0400

    Updates

---
+++ precache-manifest.f563043047b9c7fc54aa244b8e60e748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1d3434560fd65726662a7c1c84121c",
+    "url": "/unicef-material-ui/index.html"
+  },
+  {
+    "revision": "dc1ccd82d3ee78be13c3",
+    "url": "/unicef-material-ui/static/css/main.0ea3dd01.chunk.css"
+  },
+  {
+    "revision": "607ff637966e0c0f60a8",
+    "url": "/unicef-material-ui/static/js/2.00fed467.chunk.js"
+  },
+  {
+    "revision": "dc1ccd82d3ee78be13c3",
+    "url": "/unicef-material-ui/static/js/main.267d3829.chunk.js"
+  },
+  {
+    "revision": "faa47f8b657a8e570a41",
+    "url": "/unicef-material-ui/static/js/runtime~main.1dd3ad43.js"
+  },
+  {
+    "revision": "043728877790ef3246e31ef190c777f0",
+    "url": "/unicef-material-ui/static/media/unicef-logo.04372887.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5ff80d37adee3353bda31996c1ae946c.js"
+  "/unicef-material-ui/precache-manifest.f563043047b9c7fc54aa244b8e60e748.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/unicef-material-ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
