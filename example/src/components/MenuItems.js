9a9ed715ad24a7bbbe12bd2ab8fffdec41bcae22
Suresh <sevarthi.suresh@gmail.com>
2019-07-19 11:38:20 -0400

    impleamented menu tabs and items

--- example/src/components/MenuItems.js
+++ example/src/components/MenuItems.js
@@ -1,21 +1,34 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import {
   ListItemText,
   Badge,
   List,
   ListItem,
   Collapse
-} from '@material-ui/core';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
+} from '@material-ui/core'
+import MailIcon from '@material-ui/icons/Mail'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
+import StarBorder from '@material-ui/icons/StarBorder'
+
+
+const useStyles = makeStyles(theme => ({
+  paddingMenu : {
+    padding: `${theme.spacing(1, 1)} !important`,
+  },
+  nested: {
+    paddingLeft: `${theme.spacing(3)} !important`,
+  },
+}));
+
 
 
 function MenuItems() {
 
+  const classes = useStyles();
   const [openDropdown, setOpenDropdown] = React.useState(false)
 
   const handleDropdown = () => {
@@ -23,32 +36,32 @@ function MenuItems() {
   }
 
   return( 
-  <React.Fragment>
-    <ListItem button key="With Icon" >
-      <ListItemIcon> <MailIcon /> </ListItemIcon>
-      <ListItemText primary="With Icon" />
-    </ListItem>
-    <ListItem button key="Notifications">
-      <ListItemIcon> <Badge badgeContent={17} color="secondary">
-        <NotificationsIcon />
-      </Badge> </ListItemIcon>
-      <ListItemText primary="Notifications" />
-    </ListItem>
-    <ListItem button key="Dropdown" onClick={handleDropdown} >
-      <ListItemIcon>{openDropdown ? <ExpandLess /> : <ExpandMore />}</ListItemIcon>
-      <ListItemText primary="Dropdown" />
-    </ListItem>
-    <Collapse in={openDropdown} timeout="auto" unmountOnExit>
-      <List component="div" disablePadding>
-        <ListItem button>
-          <ListItemIcon>
-            <StarBorder />
-          </ListItemIcon>
-          <ListItemText primary="Starred" />
-        </ListItem>
-      </List>
-    </Collapse>
-  </React.Fragment>
+    <React.Fragment>
+      <ListItem button key="With Icon" className={classes.paddingMenu}>
+        <ListItemIcon> <MailIcon /> </ListItemIcon>
+        <ListItemText primary="With Icon" />
+      </ListItem>
+      <ListItem button key="Notifications" className={classes.paddingMenu}>
+        <ListItemIcon> <Badge badgeContent={17} color="secondary">
+          <NotificationsIcon />
+        </Badge> </ListItemIcon>
+        <ListItemText primary="Notifications" />
+      </ListItem>
+      <ListItem button key="Dropdown" onClick={handleDropdown} className={classes.paddingMenu}>
+        <ListItemIcon>{openDropdown ? <ExpandLess /> : <ExpandMore />}</ListItemIcon>
+        <ListItemText primary="Dropdown" />
+      </ListItem>
+      <Collapse in={openDropdown} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="Starred" />
+          </ListItem>
+        </List>
+      </Collapse>
+    </React.Fragment>
   )
 }
 
---
+++ example/src/components/MenuTabs.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  ListItemText,
+  ListItem,
+} from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  paddingTabs: {
+   padding: `${theme.spacing(1, 1)} !important`,
+  },
+}));
+
+
+
+export default function MenuTabs() {
+
+  const classes = useStyles();
+
+  return( 
+    <React.Fragment>
+      <ListItem button key="Active" selected className={classes.paddingTabs}>
+        <ListItemText primary="Active" />
+      </ListItem>
+      <ListItem button key="Disbaled" disabled className={classes.paddingTabs}>
+        <ListItemText primary="Disbaled" />
+      </ListItem>
+      <ListItem button key="Directory" className={classes.paddingTabs}>
+        <ListItemText primary="Directory" />
+      </ListItem>
+  </React.Fragment>
+  )
+}
