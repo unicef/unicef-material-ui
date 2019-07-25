b259e46778a48cf9246d28a239b355cdc4da8409
Suresh <sevarthi.suresh@gmail.com>
2019-07-25 17:00:32 -0400

    implemented unicef select component

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import Theme from './components/Theme'
 import UNICEFStyleProvider from './components/UNICEFStyleProvider'
 import Header from './components/Header'
 import NavTabs from './components/NavTabs'
+import SpinningButton from './components/SpinningButton'
+import IntegrationReactSelect from './components/UnicefSelect'
 
-export { Theme, UNICEFStyleProvider,  Header, NavTabs }
+export { Theme, UNICEFStyleProvider,  Header, NavTabs, SpinningButton, IntegrationReactSelect }
