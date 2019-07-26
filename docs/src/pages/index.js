8183f7f359e8f293e432d5004b9b4df3a3f9b0fc
Suresh <sevarthi.suresh@gmail.com>
2019-07-26 17:40:57 -0400

    eslint and react packages are added

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,3 +1,21 @@
-import React from "react"
+import React from "react";
+import { theme, UNICEFStyleProvider, Header } from "unicef-material-ui";
 
-export default () => <div>Hello world!</div>
+export default function App() {
+
+    return (
+        // <MuiThemeProvider theme={theme}>
+            <Header
+              applicationName="Application"
+            //   navLinks={<NavLinks />}
+            //   tabs={<NavTabs tabs={tabs} />}
+            //   // hideLogo={false}
+            //   // logoBorderLine={false}
+            //   // logo={<img alt="user" src={avatar} />}
+            //   menuItems={<MenuItems />}
+            //   menuTabs={<MenuTabs />}
+              menuButton={true}
+            />
+        // </MuiThemeProvider >
+    )
+}
