a314bf3aeb26ed27bf8ccb01239b936b1a2c7091
Suresh Sevarthi <sureshsevarthi@Sureshs-MacBook-Pro.local>
2019-07-31 11:55:25 -0400

    Updates

--- async-requires.js
+++
@@ -1,7 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---src-pages-unicef-material-ui-js": () => import("/Users/sureshsevarthi/Desktop/Workspace/unicef-material-ui/sites/src/pages/unicef-material-ui.js" /* webpackChunkName: "component---src-pages-unicef-material-ui-js" */)
-}
-
---
+++ precache-manifest.e42ba47e76dbeae263ee01a0b7ac8d59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8600c554eed0b3e1f9741ce77cd95c04",
+    "url": "/unicef-material-ui/index.html"
+  },
+  {
+    "revision": "912427a4fb0e8fa0602a",
+    "url": "/unicef-material-ui/static/css/main.6fe00705.chunk.css"
+  },
+  {
+    "revision": "8b769680acab001ab487",
+    "url": "/unicef-material-ui/static/js/2.3567a3ca.chunk.js"
+  },
+  {
+    "revision": "912427a4fb0e8fa0602a",
+    "url": "/unicef-material-ui/static/js/main.1b9a38d3.chunk.js"
+  },
+  {
+    "revision": "faa47f8b657a8e570a41",
+    "url": "/unicef-material-ui/static/js/runtime~main.1dd3ad43.js"
+  },
+  {
+    "revision": "043728877790ef3246e31ef190c777f0",
+    "url": "/unicef-material-ui/static/media/unicef-logo.04372887.svg"
+  }
+]);
