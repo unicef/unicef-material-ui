3c663258753f60b8f8d0d8717849397fa576f5d6
Suresh Sevarthi <sureshsevarthi@Sureshs-MacBook-Pro.local>
2019-10-02 09:51:24 -0400

    Updates

---
+++ precache-manifest.8b4d258220659161caf51ea8503b0f61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8b024780d407cc00e5344bd3e5104c",
+    "url": "/unicef-material-ui/index.html"
+  },
+  {
+    "revision": "152f24ff08764ce9e1e6",
+    "url": "/unicef-material-ui/static/css/main.6fe00705.chunk.css"
+  },
+  {
+    "revision": "429130a06624fc2e3d91",
+    "url": "/unicef-material-ui/static/js/2.68e02654.chunk.js"
+  },
+  {
+    "revision": "152f24ff08764ce9e1e6",
+    "url": "/unicef-material-ui/static/js/main.f3a27e4d.chunk.js"
+  },
+  {
+    "revision": "faa47f8b657a8e570a41",
+    "url": "/unicef-material-ui/static/js/runtime~main.1dd3ad43.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/unicef-material-ui/example/precache-manifest.bad60ac8bd7d002d2cb8a66f2c076272.js"
+  "/unicef-material-ui/precache-manifest.8b4d258220659161caf51ea8503b0f61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/unicef-material-ui/example/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/unicef-material-ui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
