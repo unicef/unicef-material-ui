399987056823a15e407f4e16e95a8fc30a86f8d8
Suresh <ssevarthi@unicef.org>
2020-04-23 19:33:20 +0530

    jss insertionPoint to be on head

--- src/components/UNICEFStyleProvider/UNICEFStyleProvider.js
+++ src/components/UNICEFStyleProvider/UNICEFStyleProvider.js
@@ -2,27 +2,27 @@ import React from 'react'
 import {
   StylesProvider,
   createGenerateClassName,
-  // jssPreset,
+  jssPreset,
 } from '@material-ui/styles'
-// import { create } from 'jss'
+import { create } from 'jss'
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'u',
   seed: 'Uni',
 })
 
-// const jss = create({
-//   ...jssPreset(),
-//   // Define a custom insertion point that JSS will look for when injecting the styles into the DOM.
-//   insertionPoint: 'head',
-// })
+const jss = create({
+  ...jssPreset(),
+  // Define a custom insertion point that JSS will look for when injecting the styles into the DOM.
+  insertionPoint: <head />,
+})
 
 export default function UNICEFStyleProvider(props) {
   return (
     <StylesProvider
-      injectFirst={true}
+      // injectFirst={true}
       generateClassName={generateClassName}
-      // jss={jss}
+      jss={jss}
     >
       {props.children}
     </StylesProvider>
