68da5d5313bb97cd984476a93a3a621f49cf8335
merlos <merlos@users.noreply.github.com>
2020-04-27 17:04:52 -0400

    Removed warnings of ColorsExample

--- example/src/components/ColorsExample.js
+++ example/src/components/ColorsExample.js
@@ -1,21 +1,21 @@
-import React from "react"
-import { useTheme } from "@material-ui/core/styles"
-import { Grid, Box, Paper, Typography } from "@material-ui/core"
+import React from 'react'
+import { useTheme } from '@material-ui/core/styles'
+import { Grid, Box, Paper, Typography } from '@material-ui/core'
 
 export default function ColorsExample() {
   const theme = useTheme()
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs="12">
+      <Grid item xs={12}>
         <Typography variant="h5">Unicef colors</Typography>
       </Grid>
       {Object.keys(theme.palette.unicef).map(color => {
         return (
-          <Grid item xs={6} md={4} lg={2}>
+          <Grid key={color} item xs={6} md={4} lg={2}>
             <Paper p={3}>
               <Box bgcolor={theme.palette.unicef[color]} p={4}>
-                <Typography variant="body">{color}</Typography>
+                <Typography variant="body1">{color}</Typography>
               </Box>
             </Paper>
           </Grid>
