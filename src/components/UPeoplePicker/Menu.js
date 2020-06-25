6fca6521ae815123b0d46f09cd22864bb0c17da9
merlos <merlos@users.noreply.github.com>
2020-06-25 07:43:05 -0400

    Added Typography to loading state

--- src/components/UPeoplePicker/Menu.js
+++ src/components/UPeoplePicker/Menu.js
@@ -2,11 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Box from '@material-ui/core/Box'
 import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
 
 export default function Menu({ children, selectProps, innerProps, isLoading }) {
   return (
     <Paper square className={selectProps.classes.paper} {...innerProps}>
-      {isLoading ? <Box p={2}> Loading ....</Box> : children}
+      {isLoading ? (
+        <Box p={2}>
+          <Typography>Loading...</Typography>
+        </Box>
+      ) : (
+        children
+      )}
     </Paper>
   )
 }
