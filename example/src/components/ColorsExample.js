05de7ad6528c9e421a5f503bff85ae04292e3e29
VinuGanesan <vinu.gceb@gmail.com>
2021-02-15 19:02:44 +0530

    Display all colors in example

--- example/src/components/ColorsExample.js
+++ example/src/components/ColorsExample.js
@@ -5,22 +5,46 @@ import { Grid, Box, Paper, Typography } from '@material-ui/core'
 export default function ColorsExample() {
   const theme = useTheme()
 
+  let requiredColors = [
+    'unicef',
+    'grey',
+    'text',
+    'primary',
+    'secondary',
+    'success',
+    'info',
+    'error',
+  ]
+
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Typography variant="h5">Unicef colors</Typography>
-      </Grid>
-      {Object.keys(theme.palette.unicef).map(color => {
-        return (
-          <Grid key={color} item xs={6} md={4} lg={2}>
-            <Paper p={3}>
-              <Box bgcolor={theme.palette.unicef[color]} p={4}>
-                <Typography variant="body1">{color}</Typography>
-              </Box>
-            </Paper>
-          </Grid>
-        )
-      })}
+      {requiredColors.map(
+        colorKey =>
+          (theme.palette[colorKey] && (
+            <>
+              <Grid item xs={12}>
+                <Typography
+                  style={{ textTransform: 'capitalize' }}
+                  variant="h5"
+                >
+                  {colorKey} colors
+                </Typography>
+              </Grid>
+              {Object.keys(theme.palette[colorKey]).map(color => {
+                return (
+                  <Grid key={color} item xs={6} md={4} lg={2}>
+                    <Paper p={3}>
+                      <Box bgcolor={theme.palette[colorKey][color]} p={4}>
+                        <Typography variant="body1">{color}</Typography>
+                      </Box>
+                    </Paper>
+                  </Grid>
+                )
+              })}
+            </>
+          )) ||
+          ''
+      )}
     </Grid>
   )
 }
