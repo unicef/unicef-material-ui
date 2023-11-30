a0469bb23f2716d85ce2309af43e0230a2b78a96
Deyner Lezcano <dflezcano@unicef.org>
2023-11-30 15:35:12 -0500

    add popup variant to UConfirmationButtonExample

--- src/components/UConfirmationButton/UConfirmationButton.js
+++ src/components/UConfirmationButton/UConfirmationButton.js
@@ -1,8 +1,18 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
-import { Tooltip, IconButton, Menu, MenuItem } from '@material-ui/core'
+import {
+  Tooltip,
+  IconButton,
+  Menu,
+  MenuItem,
+  Dialog,
+  DialogContent,
+  Typography,
+  DialogActions,
+} from '@material-ui/core'
 import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined'
+import UButton from '../UButton'
 
 const useStyles = makeStyles(theme => ({
   menuDelete: {
@@ -15,6 +25,12 @@ const useStyles = makeStyles(theme => ({
     marginLeft: theme.spacing(1),
   },
 }))
+
+const ControlVariants = {
+  popup: 'popup',
+  menuItem: 'menuItem',
+  icon: 'icon',
+}
 
 /**
  * UConfirmationButton is a component to get confirmation from the user before processing it.
@@ -32,25 +48,34 @@ export default function UConfirmationButton(props) {
     icon,
     confirmText,
     confirmActionText,
+    cancelText,
     ...others
   } = props
   const [deleteAnchorEl, setDeleteAnchorEl] = useState(null)
+  const [openDialog, setOpenDialog] = useState(false)
 
   const handleConfirm = e => {
     onConfirm(e, id)
-    handleDeleteOptionClicked(e)
+    if (variant !== ControlVariants.popup) handleDeleteOptionClicked(e)
   }
 
   const handleDeleteOptionClicked = e => {
     setDeleteAnchorEl(null)
   }
+
+  const onClick = e => {
+    if (variant !== ControlVariants.popup) setDeleteAnchorEl(e.currentTarget)
+    else setOpenDialog(true)
+  }
+
+  const handleCancelPopup = e => {
+    setOpenDialog(false)
+  }
+
   return (
     <React.Fragment>
       {variant === 'menuItem' ? (
-        <MenuItem
-          onClick={e => setDeleteAnchorEl(e.currentTarget)}
-          className={classes.menuLabel}
-        >
+        <MenuItem onClick={onClick} className={classes.menuLabel}>
           {icon}
           <span className={classes.span}>{buttonText}</span>
         </MenuItem>
@@ -59,7 +84,7 @@ export default function UConfirmationButton(props) {
           <IconButton
             aria-controls={`delete-confirmation-menu-${id}`}
             aria-haspopup="true"
-            onClick={e => setDeleteAnchorEl(e.currentTarget)}
+            onClick={onClick}
             disabled={!enabled}
             className={classes.menuLabel}
           >
@@ -78,6 +103,19 @@ export default function UConfirmationButton(props) {
         <MenuItem disabled>{confirmText}</MenuItem>
         <MenuItem onClick={e => handleConfirm(e)}>{confirmActionText}</MenuItem>
       </Menu>
+      <Dialog id={`delete-confirmation-menu-${id}`} open={openDialog}>
+        <DialogContent>
+          <Typography variant="body1">{confirmText || ''}</Typography>
+        </DialogContent>
+        <DialogActions>
+          <UButton onClick={handleCancelPopup} variant="uDefault">
+            {cancelText || ''}
+          </UButton>
+          <UButton onClick={handleConfirm} variant="uPrimary" color="primary">
+            {confirmActionText || ''}
+          </UButton>
+        </DialogActions>
+      </Dialog>
     </React.Fragment>
   )
 }
@@ -90,9 +128,15 @@ UConfirmationButton.propTypes = {
   /** trigger confirmation function */
   onConfirm: PropTypes.func.isRequired,
   /** variant: menu or icon */
-  variant: PropTypes.oneOf(['menuItem', 'icon']),
+  variant: PropTypes.oneOf([
+    ControlVariants.popup,
+    ControlVariants.menuItem,
+    ControlVariants.icon,
+  ]),
   /** if the variant is menuitem, this prop make sure the item enable or not */
   enabled: PropTypes.bool,
+  /** button cancel text for popup variant */
+  cancelText: PropTypes.string,
   /** button confirm text */
   confirmText: PropTypes.string,
   /** confirm action text */
@@ -107,5 +151,6 @@ UConfirmationButton.defaultProps = {
   enabled: true,
   confirmText: 'Confirm deletion?',
   confirmActionText: 'Yes, delete',
+  cancelText: 'No',
   icon: <DeleteOutlinedIcon />,
 }
