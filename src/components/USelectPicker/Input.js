ed7fc353c9ad8abd2d3ed2a8f94ceb84a01aa895
vinu.ganesan <vinu.ganesan@swordgroup.in>
2024-01-12 17:33:49 +0530

    Fix required attribute in USelectPicker

--- src/components/USelectPicker/Input.js
+++ src/components/USelectPicker/Input.js
@@ -3,6 +3,7 @@ import { components } from 'react-select'
 import PropTypes from 'prop-types'
 
 export default function Input(props) {
+  console.log(props)
   let describedBy = props['aria-describedby'] //react-select generated aria-describedby
   if (!props.hasValue) {
     const customDescribedBy = props.selectProps['aria-describedby'] //custom aria-describedby passed through the props
@@ -14,7 +15,15 @@ export default function Input(props) {
         : undefined
   }
 
-  return <components.Input {...props} aria-describedby={describedBy} />
+  return (
+    <components.Input
+      {...props}
+      aria-describedby={describedBy}
+      required={
+        (props && props.selectProps && props.selectProps.required) || undefined
+      }
+    />
+  )
 }
 
 Input.propTypes = {
